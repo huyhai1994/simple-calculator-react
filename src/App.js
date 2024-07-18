e6b77a22571002c621e7fcf85a2ae90ad5b094ec
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-18 10:46:17 +0700

    update bootstrap to form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Calculator from "./components/Calculator";
+import './App.css'
 
 function App() {
     return (
         <div className="App">
-            <h1>React Calculator</h1>
             <Calculator/>
         </div>
     );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -31,17 +31,27 @@ class Calculator extends Component {
 
     render() {
         return (<div className="container">
-            <div>
-                <input type="number" onChange={e => this.setState({number1: parseInt(e.target.value)})}/>
-                <input type="number" onChange={e => this.setState({number2: parseInt(e.target.value)})}/>
-                <button onClick={this.add}>+</button>
-                <button onClick={this.subtract}>-</button>
-                <button onClick={this.multiply}>*</button>
-                <button onClick={this.divide}>/</button>
+            <form className="bg-light p-5 rounded-3">
+                <div className="mb-3">
+                    <label htmlFor="inputNumber1">Number 1</label>
+                    <input type="number" id="inputNumber1" className="form-control"
+                           onChange={e => this.setState({number1: parseInt(e.target.value)})}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="inputNumber2">Number2</label>
+                    <input type="number" id="inputNumber2" className="form-control"
+                           onChange={e => this.setState({number2: parseInt(e.target.value)})}/>
+                </div>
+                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.add}>+</button>
+                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.subtract}>-
+                </button>
+                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.multiply}>x
+                </button>
+                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.divide}>/</button>
                 <h2>Result: {this.state.result !== undefined ? this.state.result : "Enter numbers and perform an operation"}</h2>
                 {/*TODO:
                 In the render method, you're using this.state.result without checking if it's defined. To fix this, you can initialize the result state with a default value or use conditional rendering to display a message when the result is not available:*/}
-            </div>
+            </form>
         </div>);
     }
 }
