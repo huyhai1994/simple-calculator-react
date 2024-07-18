3a4ec2da106124720c7e1497807f428690d56e01
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-18 10:57:12 +0700

    fixed auto sumit event from button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import './component.css'
 
 class Calculator extends Component {
     constructor(props) {
@@ -31,23 +32,29 @@ class Calculator extends Component {
 
     render() {
         return (<div className="container">
-            <form className="bg-light p-5 rounded-3">
+            <form className="bg-light p-5 rounded-3 calculator">
                 <div className="mb-3">
                     <label htmlFor="inputNumber1">Number 1</label>
-                    <input type="number" id="inputNumber1" className="form-control"
+                    <input type="number" id="inputNumber1" className="form-control calculator__input-number"
                            onChange={e => this.setState({number1: parseInt(e.target.value)})}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="inputNumber2">Number2</label>
-                    <input type="number" id="inputNumber2" className="form-control"
+                    <input type="number" id="inputNumber2" className="form-control calculator__input-number"
                            onChange={e => this.setState({number2: parseInt(e.target.value)})}/>
                 </div>
-                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.add}>+</button>
-                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.subtract}>-
+                <button type='button' className="btn btn-primary btn-outline-success text-white m-2"
+                        onClick={this.add}>+
                 </button>
-                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.multiply}>x
+                <button type='button' className="btn btn-primary btn-outline-success text-white m-2"
+                        onClick={this.subtract}>-
                 </button>
-                <button className="btn btn-primary btn-outline-success text-white m-2" onClick={this.divide}>/</button>
+                <button type='button' className="btn btn-primary btn-outline-success text-white m-2"
+                        onClick={this.multiply}>x
+                </button>
+                <button type='button' className="btn btn-primary btn-outline-success text-white m-2"
+                        onClick={this.divide}>/
+                </button>
                 <h2>Result: {this.state.result !== undefined ? this.state.result : "Enter numbers and perform an operation"}</h2>
                 {/*TODO:
                 In the render method, you're using this.state.result without checking if it's defined. To fix this, you can initialize the result state with a default value or use conditional rendering to display a message when the result is not available:*/}
